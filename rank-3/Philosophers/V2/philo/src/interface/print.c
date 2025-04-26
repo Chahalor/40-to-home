9a64ac87c8408868ecc7cd8625c1ab592e7f1a66
nduvoid <nathan@duvoid.fr>
2025-04-26 20:33:51 +0200

    death detection ok, we just don't stop printing at a dead

--- rank-3/Philosophers/V2/philo/src/interface/print.c
+++ rank-3/Philosophers/V2/philo/src/interface/print.c
@@ -6,7 +6,7 @@
 /*   By: nduvoid <nduvoid@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 20:25:18 by nduvoid           #+#    #+#             */
-/*   Updated: 2025/04/26 20:32:01 by nduvoid          ###   ########.fr       */
+/*   Updated: 2025/04/26 20:33:49 by nduvoid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@
 
 #include "states.h"
 
-
 #pragma endregion Headers
 #pragma region Functions
+
+// maybe not needed, juste have to check more carfully the running status before printing
 
 #pragma endregion Functions
